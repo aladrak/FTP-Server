f7b868caee870b5a7f7d249845ced5dba5be3c76
aladrak <114636165+aladrak@users.noreply.github.com>
2023-05-25 17:28:38 +0700

    Add files via upload

--- server.c
+++ server.c
@@ -7,25 +7,34 @@
 #define LEN_LOGPASS 11
 #define QTY_LOGPASS 10
 
+#define STOP 0
+#define HELP 1
+#define LOGIN 2
+#define REG 3
+#define LIST 4
+#define SEND 5 
+#define GET 6
+#define EXIT 7
+
 // Обработчик команд
 int procMsgr(char* buff) {
     if (!strncmp(buff, "-stop", 5))
-        return 0;
+        return STOP;
     if (!strncmp(buff, "-help", 5))
-        return 1;
+        return HELP;
     if (!strncmp(buff, "-login", 6))
-        return 2;
+        return LOGIN;
     if (!strncmp(buff, "-reg", 4))
-        return 3;
+        return REG;
     if (!strncmp(buff, "-list", 5))
-        return 4;
+        return LIST;
     if (!strncmp(buff, "-sendfile", 9))
-        return 5;
+        return SEND;
     if (!strncmp(buff, "-getfile", 8))
-        return 6;
+        return GET;
     if (!strncmp(buff, "-exit", 5))
-        return 7; 
-    return 8; // lucky
+        return EXIT; 
+    return INT_MAX; // lucky
 }
 
 // Остановка сервера
@@ -177,8 +186,8 @@ int authUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN
     for (int i = 0; i < counter; i++) {
         if (!strncmp(check, pass[i], LEN_LOGPASS - 1)) {
             auth = 1;
-            printf("Password %s accepted.\n", login[i]);
-            snprintf(buff, SIZE_BUF, "Password accepted. You are successfully logged in.\n");
+            printf("Password \'%s\' accepted.\n", login[i]);
+            snprintf(buff, SIZE_BUF, "Password \'%s\'accepted. You are successfully logged in.\n", login[i]);
             send(clnt, buff, strlen(buff), 0);
             break;
         }
@@ -187,8 +196,7 @@ int authUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN
     // Вывод ошибки при неверном пароле
     if (!auth) {
         printf("Password is incorrect.\n", check);
-        snprintf(buff, SIZE_BUF, "Password is incorrect. Authorization has been cancelled.\n");
-        send(clnt, buff, strlen(buff), 0);
+        sendToSock(clnt, buff, "Password is incorrect. Authorization has been cancelled.\n");
         return 0;
     }
 
@@ -206,7 +214,6 @@ int regUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN_
     }
     int len = 0;
     printf("Start of the client registration operation.\n");
-
     snprintf(buff, SIZE_BUF, "Start registration operation. Send your login [%d].\n", LEN_LOGPASS - 1);
     send(clnt, buff, strlen(buff), 0);
 
@@ -221,9 +228,7 @@ int regUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN_
         // Проверка длины (переделать)
         if (len != LEN_LOGPASS - 1) {
             printf("Invalid login length. Repeating.\n");
-
-            snprintf(buff, SIZE_BUF, "Invalid login length. Repeat sending your login.\n");
-            send(clnt, buff, strlen(buff), 0);
+            sendToSock(clnt, buff, "Invalid login length. Repeat sending your login.\n");
             continue;
         }
         
@@ -244,8 +249,7 @@ int regUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN_
     }
 
     printf("Login \'%s\' accepted.\n", login[counter]);
-
-    snprintf(buff, SIZE_BUF, "Login accepted. Send your password.\n");
+    snprintf(buff, SIZE_BUF, "Login \'%s\' accepted.\n", login[counter]);
     send(clnt, buff, strlen(buff), 0);
 
     while (1) {
@@ -259,8 +263,7 @@ int regUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN_
         // Проверка длины (переделать)
         if (len != LEN_LOGPASS - 1) {
             printf("Invalid password length. Repeating.\n");
-            snprintf(buff, SIZE_BUF, "Invalid login length. Repeat sending your password.\n");
-            send(clnt, buff, strlen(buff), 0);
+            sendToSock(clnt, buff, "Invalid login length. Repeat sending your password.\n");
             continue;
         }
 
@@ -293,8 +296,7 @@ int regUser(SOCKET clnt, char* buff, char pass[][LEN_LOGPASS], char login[][LEN_
     }
     fclose(file);
 
-    snprintf(buff, SIZE_BUF, "Password accepted. Successfully registration. Please login.\n");
-    send(clnt, buff, strlen(buff), 0);
+    sendToSock(clnt, buff, "Password accepted. Successfully registration. Please login.\n");
     return counter;
 }
 
@@ -335,6 +337,11 @@ char* getSockIp(SOCKET s){
         return NULL;
     }
     return inet_ntoa((struct in_addr)name.sin_addr);
+}
+
+void sendToSock(SOCKET clnt, char* buff, char* text) {
+    snprintf(buff, SIZE_BUF, text);
+    send(clnt, buff, strlen(buff), 0);
 }
 
 // Функция создания хоста
@@ -410,8 +417,9 @@ int main() {
         printf("New connection with %s received.\n", getSockIp(clientSocket));
 
         // Отправка сообщения клиенту
-        snprintf(buff, SIZE_BUF, "Welcome to ftp-server!\n\nSend \'-help\' to get help with server commands.\n");
-        send(clientSocket, buff, strlen(buff), 0);
+        // snprintf(buff, SIZE_BUF, "Welcome to ftp-server!\n\nSend \'-help\' to get help with server commands.\n");
+        // send(clientSocket, buff, strlen(buff), 0);
+        sendToSock(clientSocket, buff, "Welcome to ftp-server!\n\nSend \'-help\' to get help with server commands.\n");
         
         do {
             // Получение сообщения
@@ -428,29 +436,29 @@ int main() {
             // Обработка сообщений и выполнение команд
             if (buff[0] == '-') {
                 switch (procMsgr(buff)) {
-                case 0: // -stop Секретная команда
+                case STOP: // -stop Секретная команда
                     stopServ(listenSocket, clientSocket);
                     return 1;
-                case 1: // -help Вывод помощи по командам
+                case HELP: // -help Вывод помощи по командам
                     snprintf(buff, SIZE_BUF, "\n-stop\t\tStopped server.\n-help\t\tShows help-manual.\n-list\t\tList of files.\n-sendfile\tTransfer from client to server.\n-getfile\tTransfer from server to client.\n-login\t\tClient authorization.\n-reg\t\tNew client registration.\n");
                     send(clientSocket, buff, strlen(buff), 0);
                     break;
-                case 2: // -login Авторизация клиента
+                case LOGIN: // -login Авторизация клиента
                     auth_flag = authUser(clientSocket, (char*)&buff, password, login, log_counter);
                     break;
-                case 3: // -reg Регистрация нового клиента
+                case REG: // -reg Регистрация нового клиента
                     log_counter = regUser(clientSocket, (char*)&buff, password, login, log_counter);
                     break;
-                case 4: // -list Вывод каталога файлов
+                case LIST: // -list Вывод каталога файлов
                     printf("None.\n");
                     break;
-                case 5: // -sendfile Отправка файла клиент -> сервер
+                case SEND: // -sendfile Отправка файла клиент -> сервер
                     sendFile(clientSocket, (char*)&buff);
                     break;
-                case 6: // -getfile Отправка файла сервер -> клиент
+                case GET: // -getfile Отправка файла сервер -> клиент
                     printf("None.\n");
                     break;
-                case 7: // -exit Прекращение сессии
+                case EXIT: // -exit Прекращение сессии
                     printf("None.\n");
                     break;
                 default: // -
